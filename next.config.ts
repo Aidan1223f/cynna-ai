3e4960ca03c743cea5bea45a53144a78e6fdb64a
Aidan1223f <aidan.nt76@gmail.com>
2026-06-23 21:30:13 -0700

    Scope Next type checking to app

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   allowedDevOrigins: ["192.168.0.157"],
+  typescript: {
+    tsconfigPath: "tsconfig.next.json",
+  },
 };
 
 export default nextConfig;
